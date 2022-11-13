80bd90c884cc167b2a80432a1ff4b4fb88195551
qinlun <qinlun>
2022-11-13 16:45:30 +0800

    初始化项目

---
+++ src/main/java/com/example/entity/StudentInfo.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class StudentInfo {
+
+    private String studentCode;
+
+    private String name;
+
+    private int age;
+
+    private String sex;
+
+    private String address;
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author qinlun
+ * @since 2022-11-10
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 性别
+     */
+    private String sex;
+
+
+}
