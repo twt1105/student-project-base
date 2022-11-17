775a7f715aa7da4e64af8277b3107e5cc2c31651
wsy <xiaoyong_css@126.com>
2022-11-17 19:35:14 +0800

    删除多余的类

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author qinlun
@@ -19,7 +19,7 @@ import java.io.Serializable;
 @Accessors(chain = true)
 public class User implements Serializable {
 
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * 姓名
